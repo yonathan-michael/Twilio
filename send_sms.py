8fb4eeadc1b51e6f79cb78f3a2f5ecd5499162c0
Yonathan Michael <56137555+yonathan-michael@users.noreply.github.com>
2020-08-13 13:28:45 -0400

    Update send_sms.py

--- send_sms.py
+++ send_sms.py
@@ -4,8 +4,8 @@ import time
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC66e161d1bd65a29ddb53a056a38949e4'
-auth_token = '1099b3d21703543fc30a5217cfba5ccd'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 with open("DESTAexclusive.txt", "r") as f:
@@ -15,8 +15,7 @@ with open("DESTAexclusive.txt", "r") as f:
         s = list1[1].strip()
         message = client.messages \
                         .create(
-                             body="We Just Announced our next project #OutTheBlue. To our exclusive fans and friends, Thank you for rocking with us!"
-                                  "Like/Comment/Share #OutTheBlue https://instagram.com/p/CAgl-8lHWeg/ ",
+                             body="Your Message Goes Here",
                              from_='+16036051783',
                              to=s
                          )
